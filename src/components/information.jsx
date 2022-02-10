33257b0a05ad19d60e3bcde9d2a5c059bde3a34b
thomas-ken <infinite9810@outlook.com>
2022-02-10 12:29:47 -0500

    push again04

--- src/components/information.jsx
+++ src/components/information.jsx
@@ -5,7 +5,7 @@ import Mark01 from "../assets/images/rogan/mark01.png";
 export const Information = (props) => {
     return (
         <StyledContainer id='information'>
-            <Box display="flex" width="60%" border="1px solid #14f195" flexDirection="column" alignItems="center" borderRadius="10px" height="100%">
+            <Box display="flex" width="60%" border="2px solid #14f195" flexDirection="column" alignItems="center" borderRadius="10px" height="100%">
                 <Box display="flex" flex="1" width="100%" justifyContent="center">
                     <Box display="flex" flex="1" alignItems="center" justifyContent={"center"}>
                         <img src={Mark01} width="60px" height={"80px"} alt="mark" />
@@ -15,21 +15,21 @@ export const Information = (props) => {
                         <Box display="flex" flex="1" color="white" fontSize="2rem" fontWeight={"bold"} alignItems="flex-start">0.0000000008293USD (-46.39%)</Box>
                     </Box>
                 </Box>
-                <Box display="flex" flex="0.6" width="100%" color="white" borderTop="1px solid #14f195">
+                <Box display="flex" flex="0.6" width="100%" color="white" borderTop="2px solid #14f195">
                     <Box display="flex" flex="1" flexDirection="column"  >
                         <Box display="flex" flex="1" alignItems="center" justifyContent="center" fontSize="1.5rem" fontWeight={"bold"}>RANK</Box>
                         <Box display="flex" flex="1" alignItems="center" justifyContent="center" fontSize="1.5rem" fontWeight={"bold"}>4146</Box>
                     </Box>
-                    <Box display="flex" flex="1" flexDirection="column" borderLeft="1px solid #14f195">
+                    <Box display="flex" flex="1" flexDirection="column" borderLeft="2px solid #14f195">
                         <Box display="flex" flex="1" alignItems="center" justifyContent="center" fontSize="1.5rem" fontWeight={"bold"}>MARKET CAP</Box>
                         <Box display="flex" flex="1" alignItems="center" justifyContent="center" fontSize="1.5rem" fontWeight={"bold"}>$? USD</Box>
                     </Box>
-                    <Box display="flex" flex="1" flexDirection="column"  borderLeft="1px solid #14f195">
+                    <Box display="flex" flex="1" flexDirection="column"  borderLeft="2px solid #14f195">
                         <Box display="flex" flex="1" alignItems="center" justifyContent="center" fontSize="1.5rem" fontWeight={"bold"}>VOLUME</Box>
                         <Box display="flex" flex="1" alignItems="center" justifyContent="center" fontSize="1.5rem" fontWeight={"bold"}>$125.98K USD</Box>
                     </Box>
                 </Box>
-                <Box display="flex" flex="0.5" width="100%" color="white" borderTop="1px solid #14f195" fontSize="1.5rem" fontWeight={"bold"} alignItems="center" justifyContent="center" >
+                <Box display="flex" flex="0.5" width="100%" color="white" borderTop="2px solid #14f195" fontSize="1.5rem" fontWeight={"bold"} alignItems="center" justifyContent="center" >
                     Powered by CoinMarketCap
                 </Box>
             </Box>
